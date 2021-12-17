4b159398558da7f097cb166fe8cdd4e3ea757c87
edjr025 <edvaldojr025@gmail.com>
2021-12-17 14:43:50 -0300

    Metodo CREATE implementado para a Categoria

--- src/app/components/views/categoria/categoria-read/categoria-read.component.ts
+++ src/app/components/views/categoria/categoria-read/categoria-read.component.ts
@@ -1,6 +1,7 @@
 import { CategoriaService } from './../categoria.service';
 import { Component, OnInit } from '@angular/core';
 import { Categoria } from '../categoria.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-categoria-read',
@@ -13,7 +14,7 @@ export class CategoriaReadComponent implements OnInit {
 
   displayedColumns: string[] = ['id', 'nome', 'descricao', 'livro','acoes'];
 
-  constructor(private service: CategoriaService) { }
+  constructor(private service: CategoriaService, private router: Router) { }
 
   ngOnInit(): void {
     this.findAll();
@@ -25,5 +26,9 @@ export class CategoriaReadComponent implements OnInit {
       this.categoria = resposta;
    })
  }
+
+  navegarParaCategoriaCreate(){
+    this.router.navigate(["categorias/create"])
+  }
 
 }
