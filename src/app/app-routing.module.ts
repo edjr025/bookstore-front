36fb6cb5bf60aedb8e7e2cc00fce0f5c117a9605
edjr025 <edvaldojr025@gmail.com>
2021-12-18 10:42:32 -0300

    update para categoria foi implementado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { CategoriaUpdateComponent } from './components/views/categoria/categoria-update/categoria-update.component';
 import { CategoriaDeleteComponent } from './components/views/categoria/categoria-delete/categoria-delete.component';
 import { CategoriaReadComponent } from './components/views/categoria/categoria-read/categoria-read.component';
 import { HomeComponent } from './components/views/home/home.component';
@@ -24,6 +25,11 @@ const routes: Routes = [
       {
       path: 'categorias/delete/:id',
       component: CategoriaDeleteComponent
+      },
+
+      {
+        path: 'categorias/update/:id',
+        component: CategoriaUpdateComponent
       }
 
   
